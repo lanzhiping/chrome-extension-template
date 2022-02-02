21e39bd9fece8083120d59135a1c773e6f6b81fe
Zhiping Lan <zlan@expedia.com>
2022-02-02 13:32:52 +0800

    first release

---
+++ README.md
@@ -0,0 +1,37 @@
+# chrome-extension-template
+
+A template util for creating chrome extension quickly
+
+## how to use
+
+```bash
+$ npm install chrome-extension-template
+$ npx chrome-extension-template
+```
+
+this generate a simple chrome extension app in your root directory, including:
+
+```
+--
+  |-- package.json
+  |-- webpack.config.js
+  |-- src
+    |-- manifest.json
+    |-- background
+      | index.js
+    |-- options
+      | index.js
+      | index.html
+      | index.css
+    |-- popup
+      | index.js
+      | index.html
+      | index.css
+```
+
+then you can build the simple chrome extension app to load into your chrome for fun
+
+```bash
+$ npm install
+$ npm run build
+```
--- bin/index.js
+++ bin/index.js
@@ -1,60 +1,85 @@
 #!/usr/bin/env node
 
-const prompts = require('prompts')
-const validateName = require('validate-npm-package-name')
-
-const createExtensionQuestions = [
-  {
-    type: 'text',
-    name: 'name',
-    message: 'extension name?',
-    validate: (value) => {
-      const { errors } = validateName(value)
-      return errors ? JSON.stringify(errors) : true
-    }
-  },
-  {
-    type: 'text',
-    name: 'description',
-    message: 'extension description?',
-    default: ''
-  },
-  {
-    type: 'text',
-    name: 'author',
-    message: 'author name?'
-  },
-  {
-    type: 'confirm',
-    name: 'confirmed',
-    message: '',
-    onRender(color) {
-      let message = 'following files will be overriden, do you confirm?'
-      const filesWillBeOverriden = [
-        '  src/*',
-        '  webpack.config.js',
-        '  package.json'
-      ]
-
-      message +=
-        color.cyan('\n[\n') +
-        color.cyan(filesWillBeOverriden.join('\n')) +
-        color.cyan('\n]\n')
-
-      this.msg = message
-    }
-  }
-]
+const cpy = require('cpy')
+const appRoot = require('app-root-path')
+const updateJsonFile = require('update-json-file')
+const { renderToFolder } = require('template-file')
+const { readConfig } = require('./readConfig')
 
 ;(async () => {
+  const isDev = process.env.NODE_ENV === 'development'
+  const sourcePrefix = isDev ? '' : 'node_modules/chrome-extension-template/'
+  const destPrefix = isDev ? 'build/' : ''
+
   // initial config
-  const response = await prompts(createExtensionQuestions)
-  console.log(response)
+  const config = await readConfig()
+  if (!config.confirmed) {
+    return
+  }
 
-  // generate files
-  // add dependencies into package.json
-  // create template in src folder
+  // copy template other files into src folder
+  await Promise.all([
+    cpy(
+      appRoot.resolve(`${sourcePrefix}template/background`),
+      appRoot.resolve(`${destPrefix}src/background`)
+    ),
+    cpy(
+      appRoot.resolve(`${sourcePrefix}template/options`),
+      appRoot.resolve(`${destPrefix}src/options`)
+    ),
+    cpy(
+      appRoot.resolve(`${sourcePrefix}template/popup`),
+      appRoot.resolve(`${destPrefix}src/popup`)
+    ),
+    cpy(
+      appRoot.resolve(`${sourcePrefix}template/icons`),
+      appRoot.resolve(`${destPrefix}src/icons`)
+    )
+  ]).then(() => console.info('generated modules in src folder'))
+
+  // generate src/manifest.json file according to config
+  await renderToFolder(
+    appRoot.resolve(`${sourcePrefix}template/manifest.json`),
+    appRoot.resolve(`${destPrefix}src`),
+    config
+  ).then(() => console.info('generated manifest.json in src folder'))
+
   // create webpack.config.js confile
+  await renderToFolder(
+    appRoot.resolve(`${sourcePrefix}template/webpack.config.js`),
+    appRoot.resolve(destPrefix),
+    config
+  ).then(() => console.info('generated webpack.config.js in src folder'))
 
-  // run npm install to install required packages
+  // add dependencies into package.json
+  await updateJsonFile(
+    appRoot.resolve(`${destPrefix}package.json`),
+    (package) => {
+      package.devDependencies['clean-webpack-plugin'] = '^3.0.0'
+      package.devDependencies['copy-webpack-plugin'] = '^5.1.2'
+      package.devDependencies['webpack'] = '^4.46.0'
+      package.devDependencies['webpack-cli'] = '^3.3.12'
+      package.devDependencies['zip-webpack-plugin'] = '^3.0.0'
+      package.scripts['dev'] = 'webpack --watch'
+      package.scripts['build'] = 'NODE_ENV=production webpack'
+      return package
+    },
+    {
+      defaultValue: {
+        scripts: {
+          dev: 'webpack --watch',
+          build: 'NODE_ENV=production webpack'
+        },
+        devDependencies: {
+          webpack: '^4.46.0',
+          'webpack-cli': '^3.3.12',
+          'zip-webpack-plugin': '^3.0.0',
+          'copy-webpack-plugin': '^5.1.2',
+          'clean-webpack-plugin': '^3.0.0'
+        }
+      }
+    }
+  ).then(() => console.info('updated package.json'))
+
+  console.info('done')
 })()
